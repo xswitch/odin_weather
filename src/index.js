c1e1d36e49871110ee065ecd0c25ca6dbc4e90f0
xswitch <37236313+xswitch@users.noreply.github.com>
2024-04-22 22:51:56 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "./style.css";
+
+let worldView = "hello";
+
+if (worldView === "hello") {
+  worldView = "world";
+}
+const worlds = { world1: "hello", world2: "world", world3: "YEAH!" };
+console.log(worlds);
+
+console.log(`Hello ${worldView}`);
+console.log("w");
